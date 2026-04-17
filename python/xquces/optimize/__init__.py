28193048551bbcf229695e13860545ea9de39c1c
David Omanovic <117093972+davidomanovic@users.noreply.github.com>
2026-04-17 10:33:39 +0200

    Export dense Hamiltonian helpers

--- python/xquces/optimize/__init__.py
+++ python/xquces/optimize/__init__.py
@@ -1,3 +1,11 @@
+from xquces.optimize.dense_hamiltonian import (
+    build_dense_hamiltonian,
+    make_dense_hamiltonian,
+)
 from xquces.optimize.linear_method import minimize_linear_method
 
-__all__ = ["minimize_linear_method"]
+__all__ = [
+    "build_dense_hamiltonian",
+    "make_dense_hamiltonian",
+    "minimize_linear_method",
+]
