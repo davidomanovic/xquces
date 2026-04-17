bf37edabcb50203e4801e3cd7726891abdd31083
David Omanovic <117093972+davidomanovic@users.noreply.github.com>
2026-04-17 11:34:01 +0200

    Export restricted iGCR Jacobian helper

--- python/xquces/gcr/__init__.py
+++ python/xquces/gcr/__init__.py
@@ -7,6 +7,7 @@ from xquces.gcr.init import GaugeFixedGCRBalancedDFSeed
 from xquces.gcr.igcr2 import IGCR2SpinRestrictedParameterization
 from xquces.gcr.igcr3 import IGCR3SpinRestrictedParameterization
 from xquces.gcr.igcr4 import IGCR4SpinRestrictedParameterization
+from xquces.gcr.restricted_jacobian import make_restricted_gcr_jacobian
 
 __all__ = [
     "GCRAnsatz",
@@ -15,5 +16,7 @@ __all__ = [
     "gcr_from_ucj_ansatz",
     "GaugeFixedGCRBalancedDFSeed",
     "IGCR2SpinRestrictedParameterization",
-    "IGCR3SpinRestrictedParameterization",              
+    "IGCR3SpinRestrictedParameterization",
+    "IGCR4SpinRestrictedParameterization",
+    "make_restricted_gcr_jacobian",
 ]
